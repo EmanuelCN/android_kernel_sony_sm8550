e6287059cfc56ad69e905f4d387631570c7e5a6b
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2024-10-20 23:55:06 +0600

    sched/features: Disable DELAY_ZERO feature
    Since DELAY_DEQUEUE is turned off, clipping the lag on dequeue to 0 is
    pointless. Therefore, disable DELAY_ZERO, for now at least.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -32,7 +32,7 @@
  * DELAY_ZERO clips the lag on dequeue (or wakeup) to 0.
  */
 #define SCHED_FEAT_DELAY_DEQUEUE 0
-#define SCHED_FEAT_DELAY_ZERO 1
+#define SCHED_FEAT_DELAY_ZERO 0
 
 /*
  * Allow wakeup-time preemption of the current task:
