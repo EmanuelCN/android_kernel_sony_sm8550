458b5d1af7acd9b289c6cdb641622b596b097bf5
John Galt <johngaltfirstrun@gmail.com>
2025-07-12 18:31:58 +0000

    pinctrl-msm: disable irq affinity hints when SBalance is enabled

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -1489,28 +1489,33 @@ static void msm_gpio_irq_relres(struct irq_data *d)
 static int msm_gpio_irq_set_affinity(struct irq_data *d,
 				const struct cpumask *dest, bool force)
 {
-	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
-	struct msm_pinctrl *pctrl = gpiochip_get_data(gc);
 
-	unsigned int i, n_dir_conns = pctrl->n_dir_conns;
-	struct irq_data *gpio_irq_data;
-	struct msm_dir_conn *dc = NULL;
+	if (!IS_ENABLED(CONFIG_IRQ_SBALANCE)) {
+		struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
+		struct msm_pinctrl *pctrl = gpiochip_get_data(gc);
 
-	for (i = n_dir_conns; i > 0; i--) {
-		dc = &pctrl->soc->dir_conn[i];
-		gpio_irq_data = irq_get_irq_data(dc->irq);
+		unsigned int i, n_dir_conns = pctrl->n_dir_conns;
+		struct irq_data *gpio_irq_data;
+		struct msm_dir_conn *dc = NULL;
 
-		if (!gpio_irq_data || !(gpio_irq_data->chip))
-			continue;
+		for (i = n_dir_conns; i > 0; i--) {
+			dc = &pctrl->soc->dir_conn[i];
+			gpio_irq_data = irq_get_irq_data(dc->irq);
 
-		if (d->hwirq == dc->gpio)
-			return gpio_irq_data->chip->irq_set_affinity(gpio_irq_data, dest, force);
+			if (!gpio_irq_data || !(gpio_irq_data->chip))
+				continue;
+
+			if (d->hwirq == dc->gpio)
+				return gpio_irq_data->chip->irq_set_affinity(gpio_irq_data, dest, force);
+		}
+
+		if (d->parent_data && test_bit(d->hwirq, pctrl->skip_wake_irqs))
+			return irq_chip_set_affinity_parent(d, dest, force);
+
+		return 0;
+	} else {
+		return 0;
 	}
-
-	if (d->parent_data && test_bit(d->hwirq, pctrl->skip_wake_irqs))
-		return irq_chip_set_affinity_parent(d, dest, force);
-
-	return 0;
 }
 
 static int msm_gpio_irq_set_vcpu_affinity(struct irq_data *d, void *vcpu_info)
