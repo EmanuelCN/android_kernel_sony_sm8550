c45e603000c734fe1dc89da677d814d7c4ed6e0f
Shubhang Kaushik <shubhang@os.amperecomputing.com>
2026-01-21 01:31:53 -0800

    sched: Update rq->avg_idle when a task is moved to an idle CPU
    Currently, rq->idle_stamp is only used to calculate avg_idle during
    wakeups. This means other paths that move a task to an idle CPU such as
    fork/clone, execve, or migrations, do not end the CPU's idle status in
    the scheduler's eyes, leading to an inaccurate avg_idle.
    
    This patch introduces update_rq_avg_idle() to provide a more accurate
    measurement of CPU idle duration. By invoking this helper in
    put_prev_task_idle(), we ensure avg_idle is updated whenever a CPU
    stops being idle, regardless of how the new task arrived.
    
    Testing on an 80-core Ampere Altra (ARMv8) with 6.19-rc5 baseline:
     - Hackbench : +7.2% performance gain at 16 threads.
     - Schbench: Reduced p99.9 tail latencies at high concurrency.
    
    Signed-off-by: Shubhang Kaushik <shubhang@os.amperecomputing.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Tested-by: Shubhang Kaushik <shubhang@os.amperecomputing.com>
    Link: https://patch.msgid.link/20260121-v8-patch-series-v8-1-b7f1cbee5055@os.amperecomputing.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1540,6 +1540,7 @@ static inline struct cfs_rq *group_cfs_rq(struct sched_entity *grp)
 }
 #endif
 
+extern void update_rq_avg_idle(struct rq *rq);
 extern void update_rq_clock(struct rq *rq);
 
 static inline u64 __rq_clock_broken(struct rq *rq)
